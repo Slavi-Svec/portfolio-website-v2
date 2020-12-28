f0da680497f42cae5c297e13d1323fd42e1d7918
izaak svec <slavi.svec@gmail.com>
2020-12-28 20:08:49 +1100

    Complete navBar and router

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -4,10 +4,12 @@ import About from './components/About'
 import SinglePost from './components/Post'
 import Post from './components/Post'
 import Project from './components/Project'
+import NavBar from './components/NavBar'
 
 function App() {
   return (
     <BrowserRouter>
+    <NavBar />
       <Switch>
         <Route component ={Home} path='/' exact />
         <Route component ={About} path='/about' />
