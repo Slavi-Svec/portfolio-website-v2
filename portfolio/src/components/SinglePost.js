520b469a1f43017dc7096cc881a806fb04766ecc
izaak svec <slavi.svec@gmail.com>
2021-01-02 23:25:30 +1100

    create singlePost component

--- portfolio/src/components/SinglePost.js
+++ portfolio/src/components/SinglePost.js
@@ -1,5 +1,34 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import sanityClient from "../client.js";
+
+const builder = imageUrlBuilder(sanityClient);
+function urlFor(source) {
+  return builder.image(source);
+}
 
 export default function SinglePost() {
-    return <h1>SinglePost</h1>
-}
+  const [singlePost, setSinglePost] = useState(null);
+  const { slug } = useParams();
+
+  useEffect(() => {
+    sanityClient
+      .fetch(
+        `*[slug.current == "${slug}"]{
+        title,
+        _id,
+        slug,
+        mainImage{
+          asset->{
+            _id,
+              url
+            }
+          },
+          body,
+          "name": author->name,
+          "authorImage": author->image
+        }`
+      )
+      .then((data) => setSinglePost(data[0]))
+      .catch(console.error);
+  }, [slug]);
